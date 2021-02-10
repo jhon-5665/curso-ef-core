8b6e1c2210134755040e41aadd63e5f986c72dc4
Jhonatas Jardim <jhonatasjardim@hotmail.com>
2021-02-10 10:25:58 -0300

    Projeto finalizado

---
+++ CursoEFCore/Program.cs
@@ -0,0 +1,142 @@
+﻿using CursoEFCore.Domain;
+using CursoEFCore.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace CursoEFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //InserirDados();
+            //InserirDadosEmMassa();
+            //ConsultarDados();
+            //CadastrarPedido();
+            //ConsultarPedidoCarregamentoAdiantado();
+            //AtualizarDados();
+            RemoverRegistros();
+        }
+
+        private static void RemoverRegistros()
+        {
+            using var db = new Data.ApplicationContext();
+            var cliente = db.Clientes.Find(2);
+            //db.Clientes.Remove(cliente);
+            //db.Remove(cliente);
+            db.Entry(cliente).State = EntityState.Deleted;
+
+            db.SaveChanges();
+        }
+
+        private static void AtualizarDados()
+        {
+            using var db = new Data.ApplicationContext();
+            var cliente = db.Clientes.Find(1);
+            cliente.Nome = "CLiente Alterado Passo 2";
+
+            //db.Clientes.Update(cliente);
+            db.SaveChanges();
+        }
+
+        private static void ConsultarPedidoCarregamentoAdiantado()
+        {
+            using var db = new Data.ApplicationContext();
+            var pedidos = db
+                .Pedidos
+                .Include(p => p.Itens)
+                .ThenInclude(p => p.Produto)
+                .ToList();
+
+            Console.WriteLine(pedidos.Count);
+        }
+
+        private static void CadastrarPedido()
+        {
+            using var db = new Data.ApplicationContext();
+
+            var cliente = db.Clientes.FirstOrDefault();
+            var produto = db.Produtos.FirstOrDefault();
+
+            var pedido = new Pedido
+            {
+                ClienteId = cliente.Id,
+                IniciadoEm = DateTime.Now,
+                FinalizadoEm = DateTime.Now,
+                Observacao = "Pedido Teste",
+                Status = StatusPedido.Analise,
+                TipoFrete = TipoFrete.SemFrete,
+                Itens = new List<PedidoItem> 
+                { 
+                    new PedidoItem
+                    {
+                        ProdutoId = produto.Id,
+                        Desconto = 0,
+                        Quantidade = 1,
+                        Valor = 10,
+                    }
+                }
+            };
+
+            db.Pedidos.Add(pedido);
+            db.SaveChanges();
+        }
+
+        private static void ConsultarDados()
+        {
+            using var db = new Data.ApplicationContext();
+            //var consultaPorSintaxe = (from c in db.Clientes where c.Id > 0 select c).ToList();
+            var consultaPorMetodo = db.Clientes.Where(p => p.Id > 0).ToList();
+
+        }
+
+        private static void InserirDadosEmMassa()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto Teste",
+                CodigoBarras = "1234567891231",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true
+            };
+            var cliente = new Cliente
+            {
+                Nome = "Rafael Almeida",
+                CEP = "999990000",
+                Cidade = "Itabaiana",
+                Estado = "SE",
+                Telefone = "99000001111",
+            };
+
+            using var db = new Data.ApplicationContext();
+            db.AddRange(produto, cliente);
+
+            var registros = db.SaveChanges();
+            Console.WriteLine($"Total registro(s): {registros}");
+        }
+
+        private static void InserirDados()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto Teste",
+                CodigoBarras = "1234567891231",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true
+            };
+
+            using var db = new Data.ApplicationContext();
+            // db.Produtos.Add(produto);
+            // db.Set<Produto>().Add(produto);
+            // db.Entry(produto).State = EntityState.Added;
+            db.Add(produto);
+
+            var registros = db.SaveChanges();
+            Console.WriteLine($"Total registro(s): {registros}");
+        }
+    }
+}
